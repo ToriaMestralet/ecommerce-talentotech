eeb50b8ef2a69d5419697a79d05c7198c6bdf5d7
Toria <104038816+ToriaMestralet@users.noreply.github.com>
2024-12-19 20:58:45 -0300

    Actualizar app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
  
-  // Función para obtener productos de la API
+  // obtener productos de la API
   async function fetchProducts() {
     try {
-      const response = await fetch('https://fakestoreapi.com/products'); // API para obtener productos
+      const response = await fetch('https://fakestoreapi.com/products');
       const products = await response.json();
 
       renderProducts(products);
@@ -12,10 +12,10 @@
     }
   }
 
-  // Función para renderizar productos en la sección de cards
+  //renderizar productos en la sección de cards
 function renderProducts(products) {
   const slider = document.querySelector('.slider'); // Contenedor de las cards
-  slider.innerHTML = ''; // Limpiar contenido existente
+  slider.innerHTML = ''; // Limpiar
 
   products.forEach((product) => {
     const card = document.createElement('div');
@@ -33,5 +33,5 @@ function renderProducts(products) {
   });
 }
 
-  // Llamada inicial para cargar productos
+  //cargar productos
   fetchProducts();
