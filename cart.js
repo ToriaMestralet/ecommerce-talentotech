64f835146ca2cf77a1a724a548a8e647f4b86f5a
Toria <104038816+ToriaMestralet@users.noreply.github.com>
2024-12-19 19:19:57 -0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,5 +1,4 @@
-<src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> <!-- SweetAlert -->
-<script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> <!-- SweetAlert -->
   // Función para cargar el carrito desde localStorage
   function loadCart() {
     const cart = JSON.parse(localStorage.getItem('cart')) || [];
